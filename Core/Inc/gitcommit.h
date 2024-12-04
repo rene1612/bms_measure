402317aa2fe5ed2c5a79ac3268026b541eb56a89
Rene Schönrock <rene.schoenrock@online.de>
2024-12-04 21:35:18 +0100

    git commit handling für softwareinformationen

---
+++ Core/Inc/gitcommit.h
@@ -0,0 +1,5 @@
+#define GIT_INFO_PRESENT
+#define __GIT_SHORT_HASH__ 0xe806c7b
+#define __GIT_BRANCH__ "HEAD -> dev_home, tag: dev_1.0.2, github/dev_home"
+#define __GIT_DATE_STR__ "2024-11-25"
+#define __GIT_DATE_UT__ 1732571781
